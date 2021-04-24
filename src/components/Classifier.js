fd9571af9eac2c6b82e36c096575ea5754c16745
patrickhennessy-dal <80766219+patrickhennessy-dal@users.noreply.github.com>
2021-04-24 19:21:01 -0300

    Added some styling and fixed confidence level

--- src/components/Classifier.js
+++ src/components/Classifier.js
@@ -2,9 +2,6 @@ import React from 'react'
 import { useState, useEffect, useRef } from 'react'
 import * as mobilenet from "@tensorflow-models/mobilenet"
 import * as tf from '@tensorflow/tfjs'
-//import * as tfn from '@tensorflow/tfjs-node'
-//import { fileSystem } from '@tensorflow/tfjs-node/dist/io'
-//import  { bundleResourceIO } from '@tensorflow/tfjs-react-native';
 
 const Classifier = () => {
     const[isModelLoading, setIsModelLoading] = useState(false)
@@ -14,27 +11,14 @@ const Classifier = () => {
     const[resultsProb, setResultsProb] = useState([])
     const weedNames = ['Goldenrod', 'Hawkweed', 'Bunchberry', 'Sheep Sorrel', 'No Weeds', 'Hair Fescue']
 
-    //const modelJson = require("../assets/model/model.json")
-    //const modelJson = tfn.io.fileSystem("../assets/model/model.json")
-    //const modelWeight = require("../assets/model/group1-shard.bin")
-
     const imageRef = useRef()
     const fileInputRef = useRef()
 
     const loadModel = async () => {
         setIsModelLoading(true)
         try {
-        //const model = await mobilenet.load()
-        //const model = await tf.loadLayersModel(modelJson)
-        //const model = await tf.loadLayersModel('https://www.dropbox.com/sh/dih4moq4fol5hwy/AAD0U9RfZ2osKU08gbtX_SZza?dl=0%2Fmodel.json')
-        //const model = await tf.loadLayersModel('https://raw.githubusercontent.com/patrickhennessy-dal/weed_class_asabe21/master/src/assets/model/model.json')
-        //const model = await tf.loadLayersModel('https://github.com/patrickhennessy-dal/weed_class_asabe21/blob/master/src/assets/model/model.json')
-        //const model = await tf.loadLayersModel('https://173.252.4.22/model/model.json')
-        //const model = await tf.loadLayersModel('https://192.168.0.31/model/model.json')
         const model = await tf.loadLayersModel('https://cdn.jsdelivr.net/gh/patrickhennessy-dal/weed_class_asabe21/src/assets/modelMN/model.json')
-        //console.log(model.summary())
         setModel(model)
-        //console.log('Model set')
         setIsModelLoading(false)
 
         } catch(error) {
@@ -48,30 +32,24 @@ const Classifier = () => {
         if (files.length > 0) {
             const url = URL.createObjectURL(files[0])
             setImageURL(url)
-            //console.log("imageURL set")
         } else {
             setImageURL(null)
         }
     }
 
     const identify = async () => {
-        //console.log('identify')
-        //console.log(model)
         let test_img = await tf.browser.fromPixelsAsync(imageRef.current)
         test_img = tf.expandDims(test_img,0)
         test_img = tf.image.resizeBilinear(test_img, [224,224])
-        //const results = await model.classify(imageRef.current)
-        //const results = await model.predict(imageRef.current)
         const prob = await model.predict(test_img)
-        //setResults(prob)
         console.log('prob ' + prob)
         const result = tf.argMax(prob,1)
-        console.log(prob[[0]])
+        console.log(prob.dataSync()[result.dataSync()[0]])
         console.log('result ' + result)
         console.log(result.dataSync()[0])
         console.log(weedNames[result.dataSync()[0]]) 
         setResults(weedNames[result.dataSync()[0]])
-        setResultsProb(prob[result.dataSync()[0]])
+        setResultsProb(prob.dataSync()[result.dataSync()[0]])
     }
 
     const triggerUpload = () => {
@@ -91,6 +69,31 @@ const Classifier = () => {
     
     return (
         <div className='classifier'>
+            {/*<div className='inputHolder'>
+                <input
+                  type='file'
+                  accept='image/*'
+                  capture='camera'
+                  className='uploadInput'
+                  onChange={uploadImage}
+                  ref={fileInputRef}
+                />
+                <button className='ctrlBtn' onClick={triggerUpload}>Upload Image</button>
+    </div>*/}
+            <div className='mainWrapper'>
+                <div className='mainContent'>
+                    <div className='imageHolder'>
+                        {imageURL && <img src={imageURL} className='image' alt='Upload Preview' crossOrigin='anonymous' ref={imageRef}/>}
+                    </div>
+                    {results.length > 0 && <div className='resultsHolder'>
+                          <div className='result' key={results.className}>
+                            <span className='name'><p>Result: {results} </p></span>
+                            <span className='confidence'>Confidence level: {(resultsProb * 100).toFixed(2)} %</span>
+                          </div>
+                    </div>}  
+                </div>
+                {imageURL && <button className='ctrlBtn' onClick={identify}>Identify Image</button>}
+            </div>
             <div className='inputHolder'>
                 <input
                   type='file'
@@ -100,22 +103,8 @@ const Classifier = () => {
                   onChange={uploadImage}
                   ref={fileInputRef}
                 />
-                <button className='uploadImage' onClick={triggerUpload}>Upload Image</button>
-            </div>
-            <div className='mainWrapper'>
-                <div className='mainContent'>
-                    <div className='imageHolder'>
-                        {imageURL && <img src={imageURL} alt='Upload Preview' crossOrigin='anonymous' ref={imageRef}/>}
-                    </div>
-                    {results.length > 0 && <div className='resultsHolder'>
-                          <div className='result' key={results.className}>
-                            <span className='name'><p>Result: {results} </p></span>
-                            <span className='confidence'>Confidence level: {/*{(resultsProb * 100).toFixed(2)}*/}</span>
-                          </div>
-                    </div>}  
-                </div>
-                {imageURL && <button className='button' onClick={identify}>Identify Image</button>}
-            </div>
+                <button className='ctrlBtn' onClick={triggerUpload}>Upload Image</button>
+    </div>
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types' 
 
 const Header = ({ title }) => {
     return (
         <header className = 'header'>
             <h1>CNN Weed Classifier</h1>
+            <img src = '../../public/Logo_CPU.png' alt='Logo' width='42' height='42'/>
         </header>
     )
 }
